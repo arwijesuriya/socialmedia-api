fc9ce97441def12faf9652de310a6ce5d5031abc
Achira Wijesuriya <wijesuriya4me@gmail.com>
2024-06-30 06:46:32 +0530

    authController and auth routes

--- index.js
+++ index.js
@@ -2,15 +2,20 @@ const express = require("express");
 const connectDB = require("./database/db");
 const app = express();
 const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+
+// 
 const authRoute = require("./routes/auth");
 
 dotenv.config();
 app.use(express.json());
+app.use(cookieParser());
 
 // routes
 app.use("/api/auth", authRoute);
 
 
+// Start the server
 app.listen(process.env.PORT, () => {
     connectDB();
     console.log("App listening on port 6000");
--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,19 @@
 const express = require("express");
+const { registerController, loginController, logoutController, refetchUserController } = require("../controllers/authController");
 const router = express.Router();
-const User = require("../models/User");
 
-// register routes
-router.post("/register", async (req, res) => {
-    try {
-        const newUser = new User(req.body);
-        const savedUser = await newUser.save();
-        res.status(201).json(savedUser);
-    }
-    catch (error) {
-        res.status(500).json(error);
-    }
-});
+// register
+router.post("/register", registerController);
+
+// login
+router.post("/login", loginController);
+
+
+// logout
+router.get("/logout", logoutController);
+
+
+// refetch
+router.get("/refetch", refetchUserController);
 
 module.exports = router;
